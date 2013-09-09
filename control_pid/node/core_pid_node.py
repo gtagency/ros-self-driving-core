bb7e200c2cd00d05fb9cf5a39751a5516de98218
Jonathan <jonathan.suit@gmail.com>
2013-09-09 18:20:00 -0400

    pid timing

--- control_pid/node/core_pid_node.py
+++ control_pid/node/core_pid_node.py
@@ -15,7 +15,8 @@ error_integral = 0
 proportional_gain = 1
 integral_gain = 0
 derivative_gain = 0
-
+sampleTime = 1.0
+lastTime = 0.0
 
 def node():
     global correction_pub
@@ -23,7 +24,6 @@ def node():
     correction_pub = rospy.Publisher('control_correction', Float64)
     rospy.Subscriber('control_error', Float64, handle_control_error)
     rospy.Service('set_pid_gains', PidGains, set_gains)
-
     print "PID controller active"
     rospy.spin()
 
@@ -31,14 +31,21 @@ def handle_control_error(data):
     pid_correction(data.data)
 
 def pid_correction(error_angle):
-    global old_error, error_integral, proportional_gain, integral_gain, derivative_gain, correction_pub
-
-    error_differential = error_angle - old_error
-    error_integral += error_angle
-    old_error = error_angle
-    adj_error_angle = proportional_gain * error_angle + derivative_gain * error_differential + integral_gain * error_integral
-    print "Recieved error and publishing correction", error_angle, adj_error_angle
-    correction_pub.publish(adj_error_angle)
+    global old_error, error_integral, proportional_gain, integral_gain, derivative_gain, correction_pub, sampleTime, lastTime
+    
+    
+    c_time_seconds = rospy.get_time()
+    t_diff = c_time_seconds - lastTime
+    if(t_diff >= sampleTime):
+        error_differential = (error_angle - old_error)/(t_diff)
+        error_integral += (error_angle * t_diff)
+        old_error = error_angle
+        lastTime = c_time_seconds
+        adj_error_angle = proportional_gain * error_angle + derivative_gain * error_differential + integral_gain * error_integral
+        print "Recieved error and publishing correction", error_angle,adj_error_angle
+        correction_pub.publish(adj_error_angle)
+    else:
+        print "Too soon"
 
 
 def set_gains(req):
