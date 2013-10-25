a765c0c2168c73a33d3e45b6b253774f24c05ca1
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-10-25 18:08:28 -0400

    impoved image testing to allow us to publish an image right away

--- testing/pysrc/pub_image.py
+++ testing/pysrc/pub_image.py
@@ -2,6 +2,7 @@
 import rospy
 import cv
 import cv2
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from testing.srv import PublishImage
@@ -10,18 +11,25 @@ image_pub = None
 
 bridge = CvBridge()
 
-def handle_publish_image(req):
+def publish_from_file(filename):
     global bridge,image_pub
-    cv_image = cv.fromarray(cv2.imread(req.filename))
+    cv_image = cv.fromarray(cv2.imread(filename))
     image_pub.publish(bridge.cv_to_imgmsg(cv_image, "bgr8"))
-    print req.filename, "published."
+    print filename, "published."
+    
+def handle_publish_image(req):
+    publish_from_file(req.filename)
 
 def node():
     global image_pub
     rospy.init_node('pub_image')
+    filename = rospy.get_param("~image_file", None)
     s = rospy.Service('publish_image', PublishImage, handle_publish_image)
-    image_pub = rospy.Publisher('image_raw', Image)
+    image_pub = rospy.Publisher('image_raw', Image, latch=True)
     print "Ready to publish images."
+    rospy.Rate(1).sleep()
+    if filename:
+        publish_from_file(filename)
     rospy.spin()
 
 if __name__ == '__main__':
